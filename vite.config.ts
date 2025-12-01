c85c2be8277cdd3854be0c5f369293090be14881
steyn <steynrijkse@gmail.com>
2025-12-01 23:33:55 +0100

    asdfghjkl;

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'resources/js'),
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
